0595919db6f24b665ed7ed311daf69b32660ccf1
Cecil Medara <cecilm.dev@gmail.com>
2020-09-12 20:27:06 +1000

    Refactor Code to use single Add Marker function

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -14,22 +14,22 @@ export class CustomMap {
     });
   }
 
-  public addUserMarker(user: User): void {
+  public addMarker(mappable: User | Company): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
-  public addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
-      },
-    });
-  }
+  //   public addCompanyMarker(company: Company): void {
+  //     new google.maps.Marker({
+  //       map: this.googleMap,
+  //       position: {
+  //         lat: company.location.lat,
+  //         lng: company.location.lng,
+  //       },
+  //     });
+  //   }
 }
