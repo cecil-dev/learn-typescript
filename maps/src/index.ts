fccd57252a5de98371a15617c530e0446280d5c9
Cecil Medara <cecilm.dev@gmail.com>
2020-09-12 20:03:24 +1000

    POC to get the maker to set on the google maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hi there');
+import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+const user = new User();
+// const company = new Company();
+const customMap = new CustomMap('map');
+// console.log(user);
+// console.log(company);
+customMap.addUserMarker(user);
