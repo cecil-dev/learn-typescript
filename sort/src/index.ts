5c302f1caefeb308bb43ef685863fdfb0fac5698
Cecil Medara <cecilm.dev@gmail.com>
2020-09-26 20:30:18 +1000

    Basic TS sort Class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,21 +1,13 @@
-class Sorter {
-  public collection: number[] = [];
-  constructor(collection: number[]) {
-    this.collection = collection;
-  }
-
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        const leftHand = this.collection[j];
-        this.collection[j] = this.collection[j + 1];
-        this.collection[j + 1] = leftHand;
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('xDsdsdx');
+const sorterChar = new Sorter(charactersCollection);
+sorterChar.sort();
+
+console.log(charactersCollection.data);
